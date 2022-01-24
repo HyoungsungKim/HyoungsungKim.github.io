a4aace63198d26a99b1fa21a16e48d294f6569bd
Hyoungsung Kim <hyoungsung@keti.re.kr>
2022-01-24 19:10:07 +0900

    Update deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'HyoungsungKim', // Usually your GitHub org/user name.
   projectName: 'HyoungsungKim.github.io', // Usually your repo name.
+  deploymentBranch: 'master',
 
   presets: [
     [
