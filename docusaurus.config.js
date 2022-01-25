4eb3e60982d8ad11e961e40d29bee87d81caebae
Hyoungsung Kim <hyoungsung@keti.re.kr>
2022-01-25 18:28:33 +0900

    Update baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://hyoungsungkim.github.io/',
-  baseUrl: '/HyoungsungKim.github.io/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
